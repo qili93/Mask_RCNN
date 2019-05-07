5fb21db0889005d4f3141d97ef3ed2bfc21032f2
Qi Li <selmalq@gmail.com>
2019-05-07 17:33:53 +0800

    Enable LMS
    Enable LMS

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -30,6 +30,7 @@ from distutils.version import LooseVersion
 assert LooseVersion(tf.__version__) >= LooseVersion("1.3")
 assert LooseVersion(keras.__version__) >= LooseVersion('2.0.8')
 
+from tensorflow_large_model_support import LMS
 
 ############################################################
 #  Utility Functions
@@ -2360,13 +2361,14 @@ class MaskRCNN():
             workers = 0
         else:
             workers = multiprocessing.cpu_count()
-
+	
+	lms_callback = LMS()
         self.keras_model.fit_generator(
             train_generator,
             initial_epoch=self.epoch,
             epochs=epochs,
             steps_per_epoch=self.config.STEPS_PER_EPOCH,
-            callbacks=callbacks,
+            callbacks=lms_callback,
             validation_data=val_generator,
             validation_steps=self.config.VALIDATION_STEPS,
             max_queue_size=100,
